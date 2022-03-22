44df4c28ead8765518b1ddd9d1c08e337464735a
Igor Gomes <igor.firmino@corebiz.ag>
2022-03-21 21:32:14 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1968f3d5539dff1a686f039450ac8558'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8a4ddf9-4843-4457-9590-feeba058196e'
+};
